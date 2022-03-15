eb75fa4bfa46fb4ca5867025543bebdffd8624ab
jonatanb28 <jonatan.balbuena@gmail.com>
2022-03-14 22:42:01 -0300

    changes

--- api/src/db.js
+++ api/src/db.js
@@ -31,7 +31,7 @@ let sequelize =
         ssl: true,
       })
     : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/development`,
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
         { logging: false, native: false }
       );
 
