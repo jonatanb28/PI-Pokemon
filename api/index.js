a4740842f02abe9ca4487cce881f8fa9ce403828
jonatanb28 <jonatan.balbuena@gmail.com>
2022-03-18 16:52:24 -0300

    changes

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8000;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
