5b61fa7f9b0cd20db9c001cee654747ce1c150d2
jonatanb28 <jonatan.balbuena@gmail.com>
2022-03-21 17:31:22 -0300

    changes

--- api/src/app.js
+++ api/src/app.js
@@ -9,7 +9,7 @@ require('./db.js');
 const server = express();
 
 server.name = 'API';
-
+server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
