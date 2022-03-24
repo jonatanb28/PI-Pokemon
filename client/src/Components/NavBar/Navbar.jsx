8df288f439f515b77ae626c15eba89ea4c1f69bd
jonatanb28 <jonatan.balbuena@gmail.com>
2022-03-24 13:51:21 -0300

    s

--- client/src/Components/NavBar/Navbar.jsx
+++ client/src/Components/NavBar/Navbar.jsx
@@ -16,13 +16,13 @@ export default function({allTypes, handleSort, handleOrderByAttack, handleOrigin
             
                 <div>
                     <label className="navbar_label">Order by: </label>
-                    <select onChange={event => handleSort(event)}>  
+                    <select className="select_navbar" onChange={event => handleSort(event)}>  
                         <option className="option_navbar" value="ascendent">Aa to Zz</option> 
                         <option className="option_navbar" value="descendent">Zz to Aa</option> 
                     </select>
 
                     
-                    <select defaultValue ='attack' onChange={event => handleOrderByAttack(event)}>
+                    <select className="select_navbar" defaultValue ='attack' onChange={event => handleOrderByAttack(event)}>
                         <option className="option_navbar" value="attack" disabled>Attack</option>
                         <option className="option_navbar" value="ascendent">Low Attack</option> 
                         <option className="option_navbar" value="descendent">Top Attack</option> 
@@ -31,7 +31,7 @@ export default function({allTypes, handleSort, handleOrderByAttack, handleOrigin
                 
                 <div>
                     <label className="navbar_label">Filter by: </label>
-                    <select defaultValue ='Origin' onChange={event => handleOrigin(event)}>
+                    <select className="select_navbar" defaultValue ='Origin' onChange={event => handleOrigin(event)}>
                         <option className="option_navbar" value="All">Created In</option>    
                         <option className="option_navbar" value="api">Api</option> 
                         <option className="option_navbar" value="createdInDb">Data Base</option> 
