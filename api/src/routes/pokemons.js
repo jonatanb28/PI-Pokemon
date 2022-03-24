42cd7d001e95d129a52f9536a46760a5a9120a52
jonatanb28 <jonatan.balbuena@gmail.com>
2022-03-23 21:29:15 -0300

    changes

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -3,6 +3,10 @@ const {getAllPokemons} = require("./models/getAllPokemons");
 const { Pokemon, Type } = require("../db");
 
 
+router.get('/text', async (req, res)=>{
+    res.send('hola')
+})
+
 router.get('', async (req, res)=>{
     const {name} = req.query; 
     const totalPokemons = await getAllPokemons();
