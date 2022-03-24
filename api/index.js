41cd2e2a3db92ad58bbb2a1cdd19580194e8d2a3
jonatanb28 <jonatan.balbuena@gmail.com>
2022-03-23 21:58:04 -0300

    changes

--- api/index.js
+++ api/index.js
@@ -19,11 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const PORT = process.env.PORT || 8000;
+
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(PORT, () => {
-    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
+  server.listen(process.env.PORT,()=>{
+    console.log("%s listening at 3000")
   });
 });
