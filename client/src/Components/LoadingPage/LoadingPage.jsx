5b5f73505de4c7f9222bd7c605831c4a1279007e
jonatanb28 <jonatan.balbuena@gmail.com>
2022-03-24 13:56:09 -0300

    f

--- client/src/Components/LoadingPage/LoadingPage.jsx
+++ client/src/Components/LoadingPage/LoadingPage.jsx
@@ -1,26 +1,12 @@
 import React from "react";
-import styled from "styled-components";
+import './Loading.css'
 import Loading from './img/Loading.gif'
-
-
-const ContainerLoading = styled.div`
-        @import url('https://fonts.googleapis.com/css2?family=Delius&display=swap');
-        font-family: 'Delius', cursive;;
-        height: 100vh;
-        width: 100%;
-    `;
-const ImgLoading = styled.img`
-    width: 675px;
-    height: 200px;
-    position: relative;
-    top: 25rem;
-`
 
 
 export default function LoadingPage(){
     return(
-        <ContainerLoading>
-            <ImgLoading src={Loading} alt="gif" />
-        </ContainerLoading>
+        <div className="loading_container">
+            <img className="img_loading" src={Loading} alt="gif" />
+        </div>
     );
 }
